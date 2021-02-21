cc782324d8522156cb544a52e181dbff8f17a841
Christian <chrisi-g91@web.de>
2021-02-21 19:51:21 +0100

    Added "bonus tasks"

--- get-quote.py
+++ get-quote.py
@@ -9,8 +9,24 @@ def print_quote():
   
   last = len(quotes) -1
   rnd = random.randint(0,last)
+  rnd1 = random.randint(0,last)
 
-  print(quotes[rnd])
+  print(quotes[rnd],quotes[rnd1][:-1])
+  
+def write_quote():
+  #print("Keep it logically awesome.")
+
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  
+  last = len(quotes) -1
+  rnd = random.randint(0,last)
+
+  f = open("random.txt","a+")
+  f.write(quotes[rnd])
+  f.close()
 
 if __name__== "__main__":
   print_quote()
+  #write_quote()
